edb699fb6c76c82a64e462fae2848a868dee7f4c
kimkyuchul0728 <kyuchul0728g@gmail.com>
2025-05-30 17:15:14 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextInt();
+
+        System.out.println(n * (n-1) / 2);
+        System.out.println(2);
+    }
+}
