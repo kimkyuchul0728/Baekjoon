588190027c63e6e9ed88e99dd6ef9a9cda25c97e
kimkyuchul0728 <kyuchul0728g@gmail.com>
2026-02-12 02:01:41 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 92 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -6,21 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        int n = input.length();
-        int Num = 0;
-        for (int i = 0; i < n; i++) {
-            if (input.contains("dz=")) {
-                input = input.replaceFirst("dz=", " ");
-                Num++;
-                i += 2;
-            } else if (input.contains("c=") || input.contains("c-") || input.contains("d-")
-            || input.contains("lj") || input.contains("nj") || input.contains("s=") || input.contains("z=")) {
-                input = input.replaceFirst("c=|c-|d-|lj|nj|s=|z=", " ");
-                Num++;
-                i++;
-            }
-        }
-        input = input.replace(" ", "");
-        System.out.println(input.length() + Num);
+        input = input.replaceAll("dz=", "*");
+        input = input.replaceAll("c=|c-|d-|lj|nj|s=|z=", "*");
+        System.out.println(input.length());
     }
 }
