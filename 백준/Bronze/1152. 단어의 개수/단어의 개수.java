d4e6cfa4c8e50c414bd049ac8e99f382cf35e851
kimkyuchul0728 <kyuchul0728g@gmail.com>
2026-02-11 21:21:30 +0900

    [Bronze II] Title: 단어의 개수, Time: 284 ms, Memory: 52844 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,17 +1,15 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String english = scanner.nextLine();
-        String eng = english.trim();
-        if (eng.isEmpty()) {
-            System.out.println(0);
-        } else {
-            String[] i = eng.split(" ");
-            System.out.println(i.length);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine().trim();
+        if (input.equals("")) {
+            System.out.println("0");
+            System.exit(0);
         }
-        
+        System.out.println(input.split(" ").length);
     }
 }
