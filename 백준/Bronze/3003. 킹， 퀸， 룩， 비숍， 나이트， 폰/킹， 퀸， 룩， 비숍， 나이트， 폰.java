0ef074c35d1f2b9d8854fca946dfcd7bc1feefb2
kimkyuchul0728 <kyuchul0728g@gmail.com>
2026-02-11 23:19:38 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 128 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,53 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] chess = new int[6];
-        for (int i = 0 ; i < 6 ; i++) {
-            chess[i] = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = {1, 1, 2, 2, 2, 8};
+        for (int i = 0; i < 6; i++) {
+            System.out.print(arr[i] - Integer.parseInt(st.nextToken()) + " ");
         }
-        int[] count = new int[6];
-        for (int i = 0 ; i < 2 ; i++) {
-            if (chess[i] == 1) {
-                count[i] = 0;
-            } else if (chess[i] > 1) {
-                for (int j = chess[i] ; j > 1 ; j--) {
-                    count[i]--;
-                }
-            } else {
-                for (int k = chess[i] ; k < 1 ; k++) {
-                    count[i]++;
-                }
-            }
-        }
-        for (int i = 2 ; i < 5 ; i++) {
-            if (chess[i] == 2) {
-                count[i] = 0;
-            } else if (chess[i] > 2) {
-                for (int j = chess[i] ; j > 2 ; j--) {
-                    count[i]--;
-                }
-            } else {
-                for (int k = chess[i] ; k < 2 ; k++) {
-                    count[i]++;
-                }
-            }
-        }
-        if (chess[5] == 8) {
-            count[5] = 0;
-        } else if (chess[5] > 8) {
-            for (int i = chess[5] ; i > 8 ; i--) {
-                count[5]--;
-            }
-        } else {
-            for (int j = chess[5] ; j < 8 ; j++) {
-                count[5]++;
-            }
-        }
-        for (int i = 0 ; i < 5 ; i++) {
-            System.out.print(count[i] + " ");
-        }
-        System.out.print(count[5]);
     }
 }
