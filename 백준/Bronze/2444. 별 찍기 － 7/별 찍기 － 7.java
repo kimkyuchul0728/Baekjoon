22b8e31433ec0c799e83d440447efff41e193e43
kimkyuchul0728 <kyuchul0728g@gmail.com>
2026-02-11 23:48:04 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 352 ms, Memory: 20864 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,29 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int star = scanner.nextInt();
-        for (int i = 0 ; i < star ; i++) {
-            for (int j = star - i ; j > 1 ; j--) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int star = Integer.parseInt(br.readLine());
+        for (int i = 1; i < star + 1; i++) {
+            for (int j = star - i; j > 0; j--) {
                 System.out.print(" ");
             }
-            for (int k = 0 ; k < i + 1 ; k++) {
-                System.out.print("*");
-            }
-            for (int k = 0; k < i; k++) {
+            for (int k = 0; k < i * 2 - 1; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = star - 1 ; i > 0 ; i--) {
-            for (int j = star - i ; j > 0 ; j--) {
+        for (int i = 1; i < star; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for (int j = i ; j > 0 ; j--) {
-                System.out.print("*");
-            }
-            for (int j = i - 1 ; j > 0 ; j--) {
+            for (int k = star * 2 - 1 - (i * 2) ; k > 0; k--) {
                 System.out.print("*");
             }
             System.out.println();
