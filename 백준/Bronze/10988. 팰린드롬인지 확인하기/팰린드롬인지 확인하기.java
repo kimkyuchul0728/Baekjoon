05641e5c2a1fefe19e650b4a0b398cd240a5657b
kimkyuchul0728 <kyuchul0728g@gmail.com>
2026-02-12 00:36:35 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 136 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,14 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String words = scanner.next();
-        String reverseWords = new StringBuilder(words).reverse().toString();
-        if (words.equals(reverseWords)) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st = br.readLine();
+        for (int i = 0; i < st.length() / 2; i++) {
+            char A = st.charAt(i);
+            char B = st.charAt(st.length() - 1 - i);
+            if (A != B) {
+                System.out.println(0);
+                System.exit(0);
+            }
         }
+        System.out.println(1);
     }
 }
