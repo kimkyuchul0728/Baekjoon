4cca8fd290be447e5cdd054a48ded1584cad76c9
kimkyuchul0728 <kyuchul0728g@gmail.com>
2026-02-12 22:09:08 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 16244 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,24 +1,26 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[][] A = new int[9][9];
-        int max = -1;
-        int line = 0;
-        int num = 0;
-        for (int i = 0 ; i < 9 ; i++) {
-            for (int j = 0 ; j < 9 ; j++) {
-                A[i][j] = scanner.nextInt();
-                if (A[i][j] > max) {
-                    max = A[i][j];
-                    line = i + 1;
-                    num = j + 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = Integer.MIN_VALUE;
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                int nowNum = Integer.parseInt(st.nextToken());
+                if (nowNum > max) {
+                    max = nowNum;
+                    col = i + 1;
+                    row = j + 1;
                 }
             }
         }
-        System.out.println(max);
-        System.out.print(line + " ");
-        System.out.println(num);
+        System.out.println(max + "\n" + col + " " + row);
+
     }
 }
