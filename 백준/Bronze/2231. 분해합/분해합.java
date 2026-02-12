c74d1e6b65e3d43d422ee89c30c43303320800cf
kimkyuchul0728 <kyuchul0728g@gmail.com>
2026-02-12 23:59:46 +0900

    [Bronze II] Title: 분해합, Time: 112 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,23 +1,30 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        String n = N + "";
-        int M = 0;
-        for (int i = N - (n.length() * 9) ; i < N ; i++) {
-            int m = 0;
-            m = i;
-            String plus = m + "";
-            for (int j = 0 ; j < plus.length() ; j++) {
-                m += plus.charAt(j) - '0';
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int size = input.length();
+        int N = Integer.parseInt(input);
+        int min = 0;
+        for (int i = N ; i >= N - (size * 9); i--) {
+            int num = i;
+            int X = 1;
+            for (int j = 0; j < size - 1; j++) {
+                X *= 10;
             }
-            if (m == N) {
-                M = i;
-                break;
+            int sum = i;
+            while (X >= 1) {
+                sum += num / X;
+                num %= X;
+                X /= 10;
+            }
+            if (sum == N) {
+                min = i;
             }
         }
-        System.out.println(M);
+        System.out.println(min);
     }
 }
